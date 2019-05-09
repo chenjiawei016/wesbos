fcbc71d3710f943b3f09fcd15fe78018ff201438
9735d82e8c5e8e1091758785abe90d7ca63a3636 <chenjiawei@Chens-MacBook-Pro-2.local>
2019-05-09 10:40:57 +0800

    updated

---
+++ src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Inventory extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu" />
+      </div>
+    );
+  }
+}
+
+export default Inventory;
