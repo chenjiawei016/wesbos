6d79502860853f8679416815ba1cfca70d75cf94
9735d82e8c5e8e1091758785abe90d7ca63a3636 <chenjiawei@Chens-MacBook-Pro-2.local>
2019-05-09 16:52:36 +0800

    4:52pm updated 8th May

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,41 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "./sample-fishes";
+import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+
+    // reinstate our local strong
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
   addFish = fish => {
     // 1. Take a copy of the existing state
@@ -20,14 +49,52 @@ class App extends React.Component {
     });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
+  updatedFish = (key, updatedFish) => {
+    // 1. take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    // 2. Udpdate that state
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
-        <Order />
-        <Inventory addFish={this.props.addFish} />
+
+        <Order fishes={this.state.fishes} order={this.state.order} />
+
+        <Inventory
+          addFish={this.props.addFish}
+          updatedFish={this.updatedFish}
+          loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
+        />
       </div>
     );
   }
---
+++ src/components/sample-fishes.js
@@ -0,0 +1,76 @@
+const fishes = {
+  fish1: {
+    name: "Pacific Halibut",
+    image: "../public/images/hali.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "available"
+  },
+  fish2: {
+    name: "Paci Halit",
+    image: "../public/images/crab.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "available"
+  },
+  fish3: {
+    name: "Pacific Halibut",
+    image: "../public/images/lobster.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "available"
+  },
+  fish4: {
+    name: "Pacific Halibut",
+    image: "../public/images/mahi.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "unavailable"
+  },
+  fish5: {
+    name: "Pacific Halibut",
+    image: "../public/images/mussels.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "available"
+  },
+  fish6: {
+    name: "Pacific Halibut",
+    image: "../public/images/oyster.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "available"
+  },
+  fish7: {
+    name: "Pacific Halibut",
+    image: "../public/images/prawns.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "unavailable"
+  },
+  fish8: {
+    name: "Pacific Halibut",
+    image: "../public/images/salmon.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "available"
+  },
+  fish9: {
+    name: "Pacific Halibut",
+    image: "../public/images/scallops.jpg",
+    desc:
+      "Everyones favourite white fish. We will cut it to the size you need and ship it.",
+    price: 1724,
+    status: "unavailable"
+  }
+};
+
+export default fishes;
